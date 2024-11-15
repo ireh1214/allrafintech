8dc735deb7eb8ee6f7400aa631446b0d4222c1c6
hiwolfy1214 <hiwolfy1214@gmail.com>
2024-11-15 16:07:38 +0900

    빌드 오류 해결, moduleResolution 테스트

--- src/app/quotes/hooks/use-favorite-quotes.ts
+++ src/app/quotes/hooks/use-favorite-quotes.ts
@@ -2,12 +2,13 @@ import { useState, useEffect } from 'react'
 import { allQuotes } from '../data/allQuotes'
 
 export const useFavoriteQuotes = () => {
+  const [favoriteQuotes, setFavoriteQuotes] = useState<any[]>([])
   // localStorage에서 명언들을 불러옴
   // 데이터는 자동으로 삭제되지 않고, 명시적으로 삭제하지 않는 한 영구적 유지됩니다!
-  const getFavoritesFromLocalStorage = () =>
-    JSON.parse(localStorage.getItem('favoriteQuotes') || '[]')
-
-  const [favoriteQuotes, setFavoriteQuotes] = useState(getFavoritesFromLocalStorage)
+  useEffect(() => {
+    const storedFavorites = localStorage.getItem('favoriteQuotes')
+    setFavoriteQuotes(storedFavorites ? JSON.parse(storedFavorites) : [])
+  }, [])
 
   useEffect(() => {
     localStorage.setItem('favoriteQuotes', JSON.stringify(favoriteQuotes))
@@ -15,9 +16,13 @@ export const useFavoriteQuotes = () => {
 
   const toggleFavorite = (quoteId: number) => {
     setFavoriteQuotes((prevFavorites: any[]) => {
-      const isAlreadyFavorite = prevFavorites.some((quote: { id: number }) => quote.id === quoteId)
+      const isAlreadyFavorite = prevFavorites.some(
+        (quote: { id: number }) => quote.id === quoteId
+      )
       if (isAlreadyFavorite) {
-        return prevFavorites.filter((quote: { id: number }) => quote.id !== quoteId) //즐겨찾기 되어있을 시 제거
+        return prevFavorites.filter(
+          (quote: { id: number }) => quote.id !== quoteId
+        ) //즐겨찾기 되어있을 시 제거
       } else {
         const quoteToAdd = allQuotes.find((quote) => quote.id === quoteId)
         return quoteToAdd ? [...prevFavorites, quoteToAdd] : prevFavorites //추가
